7b01b92c05ff10a4b107230b95484228ded62f43
Diego <diegogarciamendez9@gmail.com>
2026-02-11 11:38:52 +0100

    tarea manolo 11/02/26

---
+++ html/js/timer/timer.js
@@ -0,0 +1,14 @@
+// document object represents the whole html page wich is
+// a three data structure
+// document.getElementById("id of an elementg in thew htmlpage")
+// this method or command allows us to retrieve an element
+// in our page by its id
+
+const timerDysplay = document.getElementById("timerlabel");
+console.debug(timerDysplay);
+const stopBtn = document.getElementById("stopBtn");
+console.debug(stopBtn);
+const resetBtn = document.getElementById("resetBtn");
+console.debug(resetBtn);
+const startBtn = document.getElementById("startBtn");
+console.debug(startBtn);
