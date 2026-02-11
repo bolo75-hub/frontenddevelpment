eb39f3eb24d17893a2a0ea3a1f74033a00f53a9f
Diego <diegogarciamendez9@gmail.com>
2026-02-11 12:58:17 +0100

    manolo tarea 11/02/26

--- html/js/timer/timer.js
+++ html/js/timer/timer.js
@@ -12,3 +12,25 @@ const resetBtn = document.getElementById("resetBtn");
 console.debug(resetBtn);
 const startBtn = document.getElementById("startBtn");
 console.debug(startBtn);
+timerDysplay.style.fontSize = "150px"; // cambiar tamaño
+
+
+startBtn.addEventListener("click", ()=>{});
+sizeAnimation();
+
+function sizeAnimation() {
+let size = 40;
+let x = 10000;
+while(x==0){
+for (let i = 0; i <50; i++) {
+    size ++;
+    timerlabel.style.frontSize = size + "px";
+}
+    for (let j = 0; j <50; j++) {
+        size --;
+        timerlabel.style.fontSize = size + "px";
+}
+x--;
+}
+}
+
